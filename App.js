eb18429f3a8f6a1d8f789bf5e45c3ac994d75294
Unknown <delowarhossaintb@yahoo.com>
2018-10-01 15:12:56 +0600

    Bottom Tab added

--- App.js
+++ App.js
@@ -1,16 +1,42 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-const myIcon = (<Icon name="rocket" size={30} color="#900" />)
+import { createBottomTabNavigator } from 'react-navigation';
+import Home from './Home';
+import Settings from './Settings';
+
+const myIcon = (<Icon name="heart-empty" size={30} color="#900" />)
+
+const BottomTab = createBottomTabNavigator(
+  {
+    Home: Home,
+    Settings: Settings,
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = 'home';
+        } else if (routeName === 'Settings') {
+          iconName = 'cog';
+        }
+        return <Icon name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: '#4ebd65',
+      inactiveTintColor: '#111',
+    },
+  }
+);
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js {myIcon}</Text>
-      </View>
+      <BottomTab />
     );
   }
 }
